0a754f348c64e41a20d07cead31a312c7bb000e4
CRISTHIAN390 <70170324+CRISTHIAN390@users.noreply.github.com>
2024-09-19 09:13:34 -0500

    subiendoenviro

---
+++ src/app/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+
+const packageInfo = require('../../package.json');
+export const environment = {
+  appVersion: packageInfo.version,
+  production: false,
+  apiUrl: 'http://127.0.0.1:4200'  // Cambia esta URL por la URL de tu API
+};
