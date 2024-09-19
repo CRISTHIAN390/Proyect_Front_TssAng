4c42c622a1c6b2401dc40eb739f1670850606b66
CRISTHIAN390 <70170324+CRISTHIAN390@users.noreply.github.com>
2024-09-19 12:28:43 -0500

    confgcarpet

--- src/app/environments/environment.prod.ts
+++ src/app/environments/environment.prod.ts
@@ -3,5 +3,5 @@ const packageInfo = require('../../package.json');
 export const environment = {
   appVersion: packageInfo.version,
   production: false,
-  apiUrl: 'http://127.0.0.1:4200'  // Cambia esta URL por la URL de tu API
+  apiUrl: 'http://127.0.0.1:4500'  // Cambia esta URL por la URL de tu API
 };
