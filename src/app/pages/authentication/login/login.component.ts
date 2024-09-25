d1dcdf292ffdd4c2ef1ffb68f061e7f06c843d8a
CRISTHIAN390 <70170324+CRISTHIAN390@users.noreply.github.com>
2024-09-25 04:46:47 -0500

    LoginConfigurado

--- src/app/pages/authentication/login/login.component.ts
+++ src/app/pages/authentication/login/login.component.ts
@@ -1,9 +1,48 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../../../services/auth.service';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
 })
 export class AppSideLoginComponent {
-  constructor() {}
+  loginForm: FormGroup;
+
+  proband:string='';
+
+  errorMessage: string = '';
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private router: Router) {
+
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      this.authService.login(this.loginForm.value).subscribe(
+        (resp: any) => {
+            localStorage.setItem('token', resp.token);
+            localStorage.setItem('email', resp.email);
+            localStorage.setItem('rol', resp.rol);
+            this.authService.updateStatusLoginService(true); // Actualiza el estado de inicio de sesión
+            this.router.navigate(['/dashboard']);
+        },
+        (error) => {
+          console.error('Error en el login', error);
+          this.errorMessage = 'Credenciales incorrectas. Vuelve a intentar';
+        }
+      );
+    } else {
+      this.errorMessage = 'Por favor, completa todos los campos correctamente.';
+    }
+  }
 }
